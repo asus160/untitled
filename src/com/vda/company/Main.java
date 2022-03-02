4fa1243e876b12932b4f6710ec96a62d5b0c63d7
Дима <asus160109@gmail.com>
2022-03-02 20:30:36 +0300

    Add 2 variables

--- src/com/vda/company/Main.java
+++ src/com/vda/company/Main.java
@@ -3,6 +3,8 @@ package com.vda.company;
 public class Main {
     static String player1;
     static String player2;
+    static String player4;
+    static String player3;
 
     public static void main(String[] args) {
 	System.out.println("Test message...");
