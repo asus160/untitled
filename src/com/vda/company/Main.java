1a28e4ca52ca49a4b28e32f52db507f2a7ae77c1
Дима <asus160109@gmail.com>
2022-03-02 20:41:33 +0300

    add objet Scanner in the project

--- src/com/vda/company/Main.java
+++ src/com/vda/company/Main.java
@@ -1,12 +1,14 @@
 package com.vda.company;
 
+import java.util.Scanner;
+
 public class Main {
-    static String player1;
-    static String player2;
-    static String player4;
-    static String player3;
+    static String playerName1;
+    static String playerName2;
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-	System.out.println("Test message...");
+        System.out.println("Player 1, please enter your name:");
+        System.out.println("Player 2, please enter your name:");
     }
 }
