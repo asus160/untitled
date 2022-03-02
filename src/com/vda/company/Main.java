961ac90926ebda7a6218df2f08dceff8aab7939a
Дима <asus160109@gmail.com>
2022-03-02 20:29:35 +0300

    Begin project

--- src/com/vda/company/Main.java
+++ src/com/vda/company/Main.java
@@ -1,6 +1,8 @@
 package com.vda.company;
 
 public class Main {
+    static String player1;
+    static String player2;
 
     public static void main(String[] args) {
 	System.out.println("Test message...");
